37ff1208df33583d5e9040ef40eef2cc37c7876d
maks <maksim.zlatkin@gmail.com>
2022-11-09 11:19:46 -0600

    Initial Commit

---
+++ dictionary-mobileapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dictionary-mobileapp
+//
+//  Created by Maksim Zlatkin on 11/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
